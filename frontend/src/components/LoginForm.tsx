1004b6fb491adaa08c1f3d0e3f8351f706e70ea5
RikkeAnkerstjerne <rikke.ankerstjerne@hotmail.com>
2024-11-12 14:51:54 +0100

    handlesubmit functionality integration with backend

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,6 +1,7 @@
 import { Input } from "./atoms/Input";
 import { PrimaryButton } from "./atoms/PrimaryButton";
 import { useState } from "react";
+import { post } from "../utils/api";
 
 type Props = {};
 
@@ -18,9 +19,26 @@ export default function LoginForm({}: Props) {
 
     console.log("Password changed to:", event.target.value);
   };
+
+  // Define the event handler for the form submission.
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Form submitted");
+
+    //    Make a POST request to the /user endpoint with the form data. The function post is imported from the utils/api module.
+    try {
+      const response = await post("/auth/login", { email, password });
+      const data = await response.json();
+      console.log("User logged in successfully!", response);
+      console.log("data-object", data);
+    } catch (error) {
+      console.error("Error login user:", error);
+    }
+  };
+
   return (
     <>
-      <form action="">
+      <form onSubmit={handleSubmit} action="">
         <Input
           onChange={handleEmailChange}
           value={email}
