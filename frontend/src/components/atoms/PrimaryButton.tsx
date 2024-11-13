69994631b6ec3651cce1f400a8fbdc4f4c652f88
RikkeAnkerstjerne <rikke.ankerstjerne@hotmail.com>
2024-11-13 14:39:32 +0100

    PrimaryButton style

--- frontend/src/components/atoms/PrimaryButton.tsx
+++ frontend/src/components/atoms/PrimaryButton.tsx
@@ -5,7 +5,10 @@ type PrimaryButtonProps = {
 
 export function PrimaryButton({ buttonText, type }: PrimaryButtonProps) {
   return (
-    <button className="bg-blue-500" type={type}>
+    <button
+      className="bg-blue text-white pl-10 pr-10 pt-2 pb-2 rounded-lg shadow-md font-bold"
+      type={type}
+    >
       {buttonText}
     </button>
   );
