ade8b7530d7d7b1932380e20c50b91065f80da9a
AstaCarl <cph-ac300@cphbusiness.dk>
2024-11-19 19:56:18 +0100

    feat: enhance button components with variant support and update navigation links in Header

--- frontend/src/components/atoms/PrimaryButton.tsx
+++ frontend/src/components/atoms/PrimaryButton.tsx
@@ -1,14 +1,26 @@
 type PrimaryButtonProps = {
   buttonText: string;
-  type: "button" | "submit" | "reset";
+  type?: "button" | "submit" | "reset";
+  href?: string;
+  variant: "primary" | "secondary";
 };
 
-export function PrimaryButton({ buttonText, type }: PrimaryButtonProps) {
+export function PrimaryButton({ buttonText, type = "button", href, variant }: PrimaryButtonProps) {
+  const variantClasses = {
+    primary: "bg-blue text-white",
+    secondary: "bg-white text-blue border border-gray",
+  };
+
+  if (href) {
+    return (
+      <a href={href} className={`pl-10 pr-10 pt-2 pb-2 rounded-lg shadow-md font-bold ${variantClasses[variant]}`}>
+        {buttonText}
+      </a>
+    );
+  }
+
   return (
-    <button
-      className="bg-blue text-white pl-10 pr-10 pt-2 pb-2 rounded-lg shadow-md font-bold"
-      type={type}
-    >
+    <button className={`pl-10 pr-10 pt-2 pb-2 rounded-lg shadow-md font-bold ${variantClasses[variant]}`} type={type}>
       {buttonText}
     </button>
   );
