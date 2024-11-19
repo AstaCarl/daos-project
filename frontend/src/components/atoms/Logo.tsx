1b9bcca344beb3a22b7c44a583e012c0d503b957
AstaCarl <cph-ac300@cphbusiness.dk>
2024-11-19 21:52:47 +0100

    fix: wrap logo text in a link for better navigation

--- frontend/src/components/atoms/Logo.tsx
+++ frontend/src/components/atoms/Logo.tsx
@@ -1,12 +1,12 @@
 export function Logo() {
   return (
-    <>
+    <a href="/">
       <h1 className="font-display font-medium text-red text-xl">
         Musik Samspil
       </h1>
       <p className="font-sans text-blue text-xs">
         Skabt af DAOS - Dansk amatørorkester Samvirke
       </p>
-    </>
+      </a>
   );
 }
