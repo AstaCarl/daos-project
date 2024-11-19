ec00d978db75c93b686e7de47118d23019f84566
AstaCarl <cph-ac300@cphbusiness.dk>
2024-11-19 12:57:40 +0100

    quick fix for header, height

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -23,7 +23,7 @@ export default function Header({}: Props) {
         <div>
           <ul
             className={`absolute w-full top-20 pt-[32px] pb-[24px] left-0 flex flex-col gap-3 items-center transform transition:transition bg-white ${
-              isMenuOpen ? "opacity-100" : "opacity-0"
+              isMenuOpen ? "opacity-100 h-fit" : "opacity-0 h-0"
             }`}
           >
             <li>
