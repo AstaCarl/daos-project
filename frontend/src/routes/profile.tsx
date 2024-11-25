2c30e45d63a24605e5e2eb039ac821a2153986c8
RikkeAnkerstjerne <rikke.ankerstjerne@hotmail.com>
2024-11-25 11:36:47 +0100

    profile header, add user name and styling

--- frontend/src/routes/profile.tsx
+++ frontend/src/routes/profile.tsx
@@ -2,69 +2,66 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useAuthStore from "../hooks/store/auth-store";
 import CreateCard from "../components/CreateCard";
-import CreateEmsembleForm  from "../components/CreateEmsembleForm";
+import CreateEmsembleForm from "../components/CreateEmsembleForm";
 import { useFetch } from "../hooks/use-fetch";
+import ProfileHeader from "../components/ProfileHeader";
 
 export default function profile({}) {
-    const navigate = useNavigate();
-    // Get the isLoggedIn state variable and the login function from the auth store
-    const { isLoggedIn } = useAuthStore();
-    const [openEmsenbleForm, setOpenEnsembleForm] = useState(false);
+  const navigate = useNavigate();
+  // Get the isLoggedIn state variable and the login function from the auth store
+  const { isLoggedIn } = useAuthStore();
+  const [openEmsenbleForm, setOpenEnsembleForm] = useState(false);
 
-    const handleOpenEnsembleForm = () => {
-      console.log("Open ensemble form")
-      setOpenEnsembleForm(true);
-    };
+  const handleOpenEnsembleForm = () => {
+    console.log("Open ensemble form");
+    setOpenEnsembleForm(true);
+  };
 
-    useEffect(() => {
-      // Redirect to login if the user is not logged in
-      if (!isLoggedIn) {
-        navigate("/login");
+  useEffect(() => {
+    // Redirect to login if the user is not logged in
+    if (!isLoggedIn) {
+      navigate("/login");
+    }
+  }, [isLoggedIn, navigate]);
+
+  useEffect(() => {
+    getEnsemble();
+  }, []);
+
+  const getEnsemble = async () => {
+    try {
+      const user = localStorage.getItem("user");
+      if (!user) {
+        throw new Error("User is missing");
       }
-      
-    }, [isLoggedIn, navigate]);
+      const userId = JSON.parse(user)._id;
 
-    useEffect(() => {
- getEnsemble();
-    }, []);
+      const response = await useFetch(`/ensemble/${userId}`, "GET", {
+        "Content-Type": "application/json",
+      });
 
-    const getEnsemble = async () => {
-      try {
-        const user = localStorage.getItem("user");
-        if (!user) {
-          throw new Error("User is missing");
-        }
-        const userId = JSON.parse(user)._id;
-
-          const response = await useFetch(
-            `/ensemble/${userId}`,
-            "GET",
-            {
-              "Content-Type": "application/json",
-            },
-          );
-    
-          if (response.ok) {
-            const data = await response.json();
-            console.log("Create ensemble successful:", data);
-          } else {
-            console.error("Create ensemble error:", response.statusText);
-          }
-        } catch (error) {
-          console.error("Create ensemble error:", error);
-        }
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Create ensemble successful:", data);
+      } else {
+        console.error("Create ensemble error:", response.statusText);
       }
+    } catch (error) {
+      console.error("Create ensemble error:", error);
+    }
+  };
 
   return (
     <div>
-    <CreateCard
-    buttonText="Opret ensemble"
-    paragrafText="Hvis du repræsenterer et ensemble kan du oprette det her, så du kan lave et opslag på vegne af ensemblet."
-    subtitle="Mine ensembler"
-    smallButtonText="Tilføj" onClick={handleOpenEnsembleForm} />
-    {openEmsenbleForm &&
-    <CreateEmsembleForm/>
-  }
+      <ProfileHeader />
+      <CreateCard
+        buttonText="Opret ensemble"
+        paragrafText="Hvis du repræsenterer et ensemble kan du oprette det her, så du kan lave et opslag på vegne af ensemblet."
+        subtitle="Mine ensembler"
+        smallButtonText="Tilføj"
+        onClick={handleOpenEnsembleForm}
+      />
+      {openEmsenbleForm && <CreateEmsembleForm />}
     </div>
-  )
+  );
 }
