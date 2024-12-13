0e967162d7aef6e8318a4971e68b2bd176955bbd
AstaCarl <cph-ac300@cphbusiness.dk>
2024-12-13 12:09:44 +0100

    validation for posts, backend and display of the errors on frontend

--- backend/src/posts/dto/create-post.dto.ts
+++ backend/src/posts/dto/create-post.dto.ts
@@ -1,11 +1,16 @@
 import { Transform } from "class-transformer";
+import { IsNotEmpty } from "class-validator";
 import { Types } from "mongoose";
 
 export class CreatePostDto {
+    @IsNotEmpty()
     title: string;
+    @IsNotEmpty()
     description: string;
+    @IsNotEmpty()
     instrument: Types.ObjectId;
-
+    @IsNotEmpty()
     user: string;
+    @IsNotEmpty()
     ensemble: string;
 }
