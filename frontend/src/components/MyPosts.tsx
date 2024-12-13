097c9becf89d098794a26d20eb756550fc95ce7c
AstaCarl <cph-ac300@cphbusiness.dk>
2024-12-13 09:05:26 +0100

    Refactor MyPosts component layout for improved responsiveness and styling

--- frontend/src/components/MyPosts.tsx
+++ frontend/src/components/MyPosts.tsx
@@ -26,40 +26,41 @@ export default function MyPosts({ posts, handlePostsOpen }: Props) {
             />
           </div>
         </div>
-
-        {posts &&
-          posts.map((post: Posts, index: number) => (
-            <React.Fragment key={index}>
-              <div className="flex flex-col border border-accent-grey w-full rounded-md">
-                <div className="flex flex-col gap-2 border border-accent bg-gray-50 rounded-sm p-3">
-                  <div className="flex w-full gap-4">
-                    <Image className="aspect-square object-cover w-[15%] rounded-xl border-2 border-white shadow-md" />
-                    <div>
-                      <Title variant="red" title={post.ensemble.title} />
-                      <div className="flex gap-4">
-                        <Paragraf
-                          variant="body"
-                          className="font-bold"
-                          paragrafText={post.ensemble.city}
-                        />
-                        <Paragraf
-                          variant="body"
-                          paragrafText={`${post.ensemble.activeUsers.length} musikkere`}
-                        />
+        <div className="flex overflow-x-auto space-x-4">
+          {posts &&
+            posts.map((post: Posts, index: number) => (
+              <React.Fragment key={index}>
+                <div className="flex flex-col min-w-[280px] space-x border  border-accent-grey w-full rounded-md">
+                  <div className="flex flex-col  gap-2 border border-accent bg-gray-50 rounded-sm p-3">
+                    <div className="flex w-full gap-4">
+                      <Image className="aspect-square object-cover w-[15%] rounded-xl border-2 border-white shadow-md" />
+                      <div>
+                        <Title variant="red" title={post.ensemble.title} />
+                        <div className="flex gap-4">
+                          <Paragraf
+                            variant="body"
+                            className="font-bold"
+                            paragrafText={post.ensemble.city}
+                          />
+                          <Paragraf
+                            variant="body"
+                            paragrafText={`${post.ensemble.activeUsers.length} musikkere`}
+                          />
+                        </div>
                       </div>
                     </div>
                   </div>
+                  <div className="flex flex-col p-4 gap-3">
+                    <Subtitle variant="cardTitle" subtitle={post.title} />
+                    <Subtitle
+                      variant="instrument"
+                      subtitle={post.instrument.name}
+                    />
+                  </div>
                 </div>
-                <div className="flex flex-col p-4 gap-3">
-                  <Subtitle variant="cardTitle" subtitle={post.title} />
-                  <Subtitle
-                    variant="instrument"
-                    subtitle={post.instrument.name}
-                  />
-                </div>
-              </div>
-            </React.Fragment>
-          ))}
+              </React.Fragment>
+            ))}
+        </div>
       </div>
     </section>
   );
