40618e4079e09cce144d9e28f3e997a79f7b7bb6
AstaCarl <cph-ac300@cphbusiness.dk>
2024-12-16 10:22:22 +0100

    Comments for the main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,22 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 
-
+// Main function to bootstrap the application(start the application)
 async function bootstrap() {
-  dotenv.config();
-
+ // Create the application, from NestFactory using the AppModule
   const app = await NestFactory.create(AppModule);
+
   // Enable validation globally
   app.useGlobalPipes(new ValidationPipe());
-      // Enable CORS, to give the frontend access to the API
-      app.enableCors({
-        origin: 'http://localhost:5173', // URL of the port frontend is running on
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        credentials: true,
-      });
-  
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Enable CORS, to give the frontend access to the API
+  app.enableCors({
+    origin: 'http://localhost:5173', // URL of the port frontend is running on
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Methods allowed
+    credentials: true,
+  });
+
+  // Start the application on port 3000
+  await app.listen(3000);
 }
+// Call the main function to start the application
 bootstrap();
