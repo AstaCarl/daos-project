cf3b44f24bd231dec91f5f8ec08ea4af909e156b
AstaCarl <cph-ac300@cphbusiness.dk>
2024-12-17 11:28:15 +0100

    auth comments

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -7,10 +7,11 @@ import { JwtModule } from '@nestjs/jwt';
 @Module({
   imports: [
     UserModule,
+    // import JwtModule
     JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '24h' },
+      global: true, // makes the JWT module globally available
+      secret: process.env.JWT_SECRET, // secret key for signing JWT token
+      signOptions: { expiresIn: '24h' }, // token expiration time
     }),
   ],
   controllers: [AuthController],
