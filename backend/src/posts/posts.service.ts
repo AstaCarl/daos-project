f6794cfc97de71cdaa90a4aa29dc166621971426
AstaCarl <cph-ac300@cphbusiness.dk>
2025-01-02 15:55:35 +0100

    return [ ] if no posts

--- backend/src/posts/posts.service.ts
+++ backend/src/posts/posts.service.ts
@@ -8,31 +8,46 @@ import { Post } from './schema/post.schema';
 export class PostsService {
   constructor(@InjectModel(Post.name) private postModel: Model<Post>) {}
 
-// Method to create a new post
+  // Method to create a new post
   createPost(createPostDto: CreatePostDto) {
     const createdPost = new this.postModel(createPostDto);
     return createdPost.save();
   }
 
   // Method to get all posts, and populate the instrument, ensemble, and user fields, and sort by createdAt newest first
-  findAll() {
-   return this.postModel.find().populate('instrument').populate('ensemble').populate('user').sort({ createdAt: -1 }).exec();
+  async findAll() {
+    const posts = await this.postModel
+      .find()
+      .populate('instrument')
+      .populate('ensemble')
+      .populate('user')
+      .sort({ createdAt: -1 })
+      .exec();
+    if (posts.length === 0) {
+      return [];
+    } else {
+      return posts;
+    }
   }
 
   // Method to find a post by user id, and populate the instrument, ensemble, and user fields, and sort by createdAt newest first.
   async findOneByUserId(id: string) {
-
     // Create a new ObjectId from the id string
     const objectId = new Types.ObjectId(id);
 
     // Find all posts that have a user field that matches the objectId
-    const posts = await this.postModel.find({ user: objectId }).populate('instrument').populate('ensemble').populate('user').sort({ createdAt: -1 }).exec();
+    const posts = await this.postModel
+      .find({ user: objectId })
+      .populate('instrument')
+      .populate('ensemble')
+      .populate('user')
+      .sort({ createdAt: -1 })
+      .exec();
 
     // return an empty array if no posts are found
     if (posts.length === 0) {
       return [];
-    }
-    else {
+    } else {
       return posts;
     }
   }
